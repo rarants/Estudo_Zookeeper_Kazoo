93ebd8c4b9e601e1650a952295cc47741bc795f1
System Administrator <root@Rafaels-MacBook-Pro.local>
2020-12-03 01:48:34 -0300

    Atualizando arquivo

--- examples/Test.py
+++ examples/Test.py
@@ -4,31 +4,37 @@ import psutil
 
 
 
-def node_exists(path):
-    # Determine if a node exists
-    if zk.exists(path):
-        print("Founded znode = " + path)
-    else:
-        print("znode not found")
-
-
-
 zk = KazooClient(hosts='127.0.0.1:2181')
 zk.start()
-#zk.create("/memory_usage/", b"oi")
-#zk.create("/cpu_usage/")
-#zk.create("/disk_usage/")
-#zk.create("/process_number/")
-memory_usage = str(psutil.virtual_memory())
-memory_usage = memory_usage.encode()
-zk.set("/memory_usage/", b"" + memory_usage)
-data, stat = zk.get("/memory_usage")
-print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
-# Check if nodes exists
-node_exists("/memory_usage")
-node_exists("/cpu_usage")
-node_exists("/disk_usage")
-node_exists("/process_number")
+@zk.DataWatch('/memory_usage/')
+def my_func(data, stat):
+    print("Data is %s" % data)
+    print("Version is %s" % stat.version)
+
+
+while True:
+    memory_usage = (str(psutil.virtual_memory())).encode()#Obtem os dados, transforma em string, codifica para bits
+    cpu_usage = (str(psutil.cpu_percent(1, percpu=False))).encode()
+    disk_usage = (str(psutil.disk_usage('/'))).encode()
+    process_number =  (str(len(psutil.pids()))).encode()
+    zk.set("/memory_usage/", b"" + memory_usage)
+    zk.set("/cpu_usage/", b"" + cpu_usage)
+    zk.set("/disk_usage/", b"" + disk_usage)
+    zk.set("/process_number/", b"" + process_number )
+
+    #data, stat = zk.get("/memory_usage")
+    #print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+    #data, stat = zk.get("/cpu_usage")
+    #print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+    #data, stat = zk.get("/disk_usage")
+    #print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+    #data, stat = zk.get("/process_number")
+    #print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+
+
+    time.sleep(5)
+
+
 
 zk.stop()
 
